a11dc839552a5918bce9d687a190586488b88002
nuiva <william.paavola@gmail.com>
2020-01-20 18:00:37 +0200

    Added detection of meditation talents.

--- LibManaTicks/LibManaTicks.lua
+++ LibManaTicks/LibManaTicks.lua
@@ -1,10 +1,7 @@
 
 if LibManaTicks ~= nil then return end
-local blockedClasses = {
-	["WARRIOR"] = true,
-	["ROGUE"] = true,
-}
-if blockedClasses[select(2, UnitClass("player"))] then return end
+local _, playerClass = UnitClass("player")
+if playerClass == "WARRIOR" or playerClass == "ROGUE" then return end
 
 local batchWindow = 0.5 -- seconds, must include padding for batch timing error
 local tickInterval = 2.03 -- seconds, mana tick interval
@@ -13,7 +10,28 @@ local spellcastBlockLength = 5 -- seconds, time until ticks are allowed after sp
 local energizeRejectUntil = 0
 local castBlockUntil = 0
 local predictedTick = 1/0 -- = inf
+local meditationTalent = false
+local meditationGear = false
+local meditationBuffs
 local triggers = {}
+local meditation = {
+	talents = {
+		["PRIEST"] = {1,8}, -- Meditation, verify!
+		["DRUID"] = {3,6}, -- Reflection, verify!
+		["MAGE"] = {1,12}, -- Arcane Meditation, verify!
+	},
+	buffs = {
+		[15271] = true, -- Spirit Tap
+		[18371] = true, -- Soul Siphon from Improved Drain Soul talent
+		[23684] = true, -- Aura of the Blue Dragon from Darkmoon Card: Blue Dragon
+		[29166] = true, -- Innervate
+	},
+	state = {
+		talent = false,
+		buff = false,
+		gear = false,
+	}
+}
 
 LibManaTicks = {
 	version = "1.0",
@@ -41,15 +59,56 @@ local function UpdateMana()
 	return diff
 end
 
-local function IsRegenBlocked()
-	local t = GetTime()
-	return t < energizeRejectUntil or t < castBlockUntil
-end
-
 local function ManaTick(isReal)
 	TriggerEvent("ManaTickAlways")
 	if isReal then TriggerEvent("ManaTick") end
 	predictedTick = GetTime() + tickInterval
+end
+
+local function CheckTalents()
+	local t = meditation.talents[playerClass]
+	if t == nil then return end
+	local points = select(5, GetTalentInfo(unpack(t)))
+	meditation.state.talent = points > 0
+	return meditation.state.talent
+end
+
+local function CheckBuffs()
+	local i = 1
+	while true do
+		local buffId = select(10, UnitBuff("player", i))
+		if buffId == nil then return false end
+		if meditation.buffs[buffId] then
+			meditation.state.buff = true
+			return true
+		end
+		i = i + 1
+	end
+end
+
+local function CheckGear()
+	for i = 1,19 do
+		local item = GetInventoryItemLink("player", i)
+		if item then
+			local stats = GetItemStats(item)
+			if stats then
+				local mp5 = stats["ITEM_MOD_POWER_REGEN0_SHORT"]
+				if mp5 and mp5 > 0 then
+					meditation.state.gear = true
+					return true
+				end
+			end
+		end
+	end
+	meditation.state.gear = false
+	return false
+end
+
+local function IsRegenBlocked()
+	if meditation.state.talent or meditation.state.gear then return false end
+	if CheckBuffs() then return false end
+	local t = GetTime()
+	return t < energizeRejectUntil or t < castBlockUntil
 end
 
 local batch = {
@@ -73,6 +132,8 @@ f:SetScript("OnEvent", function(self, e, ...)
 		if manadiff <= 0 then return end
 		if IsRegenBlocked() then return end
 		ManaTick(true)
+	elseif e == "CHARACTER_POINTS_CHANGED" or e == "PLAYER_ENTERING_WORLD" then
+		CheckTalents()
 	end
 end)
 
@@ -94,3 +155,5 @@ end)
 f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 f:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
 f:RegisterEvent("UNIT_POWER_UPDATE")
+f:RegisterEvent("CHARACTER_POINTS_CHANGED")
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
