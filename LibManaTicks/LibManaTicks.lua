c82bdd251603bc7d3ae0db0478b50e4d06932c49
nuiva <william.paavola@gmail.com>
2020-01-20 12:42:29 +0200

    Initial commit.

---
+++ LibManaTicks/LibManaTicks.lua
@@ -0,0 +1,99 @@
+
+if LibManaTicks ~= nil then return end
+local blockedClasses = {
+	["WARRIOR"] = true,
+	["ROGUE"] = true,
+}
+if blockedClasses[select(2, UnitClass("player"))] then return end
+
+local batchWindow = 0.5 -- seconds, must include padding for batch timing error
+local tickInterval = 2.03 -- seconds, mana tick interval
+local spellcastBlockLength = 5 -- seconds, time until ticks are allowed after spellcast
+
+local energizeRejectUntil = 0
+local castBlockUntil = 0
+local predictedTick = 1/0 -- = inf
+local triggers = {}
+
+LibManaTicks = {
+	version = "1.0",
+}
+
+function LibManaTicks.RegisterCallback(self, e, f)
+	if triggers[e] == nil then
+		triggers[e] = {}
+	end
+	table.insert(triggers[e], f)
+end
+
+function TriggerEvent(e)
+	local a = triggers[e]
+	if a == nil then return end
+	for _,v in pairs(a) do
+		v(e)
+	end
+end
+
+local playerMana = nil
+local function UpdateMana()
+	local mana = UnitPower("player", 0)
+	local diff = mana - (playerMana or mana)
+	playerMana = mana
+	return diff
+end
+
+local function IsRegenBlocked()
+	local t = GetTime()
+	return t < energizeRejectUntil or t < castBlockUntil
+end
+
+local function ManaTick(isReal)
+	TriggerEvent("ManaTickAlways")
+	if isReal then TriggerEvent("ManaTick") end
+	predictedTick = GetTime() + tickInterval
+end
+
+local batch = {
+	t = 0,
+	spellCast = false,
+	lostMana = false,
+}
+
+local f = CreateFrame("Frame")
+
+f:SetScript("OnEvent", function(self, e, ...)
+	if e == "COMBAT_LOG_EVENT_UNFILTERED" then
+		local _, a, _, _, _, _, _, targetGUID = CombatLogGetCurrentEventInfo()
+		if a ~= "SPELL_ENERGIZE" or targetGUID ~= UnitGUID("player") then return end
+		local t = GetTime()
+		energizeRejectUntil = t + batchWindow
+	elseif e == "UNIT_SPELLCAST_SUCCEEDED" then
+		batch.spellCast = true
+	elseif e == "UNIT_POWER_UPDATE" then
+		local manadiff = UpdateMana()
+		if manadiff < 0 then batch.lostMana = true end
+		if manadiff <= 0 then return end
+		if IsRegenBlocked() then return end
+		ManaTick(true)
+	end
+end)
+
+f:SetScript("OnUpdate", function()
+	local t = GetTime()
+	if batch.spellCast and batch.lostMana then
+		castBlockUntil = t + spellcastBlockLength
+		TriggerEvent("Spellcast")
+	end
+	batch = {
+		t = t,
+		spellCast = false,
+		lostMana = false,
+	}
+	if predictedTick <= t then
+		ManaTick(false)
+	end
+end)
+
+f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+f:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
+f:RegisterEvent("UNIT_POWER_UPDATE")
---
+++ README.md
@@ -0,0 +1,21 @@
+# LibManaTicks
+
+LibManaTicks is a *World of Warcraft: Classic* interface addon that provides events for the player's natural mana gains ("mana ticks").
+
+## Interface
+
+LibManaTicks works through the function
+```LibManaTicks:RegisterCallback(event, function)```
+The `event` parameter must be one of
+* `ManaTick`: Occurs when the player gains a natural mana tick.
+* `ManaTickAlways`: Occurs when the player would gain mana, even if the tick is blocked by casting a spell. This also occurs whenever `ManaTick` does.
+* `Spellcast`: Occurs when the player casts a spell that interrupts mana ticks, that is, costs mana.
+
+## WeakAuras
+
+Since LibManaTicks has no graphical interface, you will need another addon or a WeakAura to properly use it. The file `WeakAura.txt` contains a WeakAura that shows the mana ticks and spellcast as progress bars.
+
+## How does it work?
+
+LibManaTicks tracks player mana changes using the event `UNIT_POWER_UPDATE`. If the player mana goes down and a simultaneous `UNIT_SPELLCAST_SUCCEEDED`, mana ticks are blocked for 5 seconds. If the player mana goes up and a preceding combat log event `SPELL_ENERGIZE` is detected, then that mana gain is blocked. Mana gains that are not blocked are always interpreted to be mana ticks.
+
