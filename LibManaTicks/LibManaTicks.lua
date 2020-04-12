af1608b67ed77580aaf38a3cd246f032d17e1898
nuiva <34218315+nuiva@users.noreply.github.com>
2020-04-12 13:21:20 +0300

    ManaTick now triggers for energy, ManaTickAlways for health.

--- LibManaTicks/LibManaTicks.lua
+++ LibManaTicks/LibManaTicks.lua
@@ -3,11 +3,12 @@ if LibManaTicks ~= nil then return end
 local _, playerClass = UnitClass("player")
 local playerGUID = UnitGUID("player")
 
-local batchWindow = 0.4 -- seconds, spell batch length
+local batchWindow = 0.4053 -- seconds, spell batch length
 local batchError = 0.1 -- seconds, error tolerance for batch delays
-local tickInterval = 2.03 -- seconds, mana tick interval
+local tickInterval = 5 * batchWindow -- seconds, mana tick interval
 
 local energizeRejectUntil = 0
+local healRejectUntil = 0
 local predictedTick = 1/0 -- = inf
 local triggers = {}
 
@@ -22,10 +23,6 @@ function LibManaTicks.RegisterCallback(self, e, f)
 	table.insert(triggers[e], f)
 end
 
-if playerClass == "WARRIOR" or playerClass == "ROGUE" then
-	return
-end
-
 local function TriggerEvent(e)
 	if triggers[e] == nil then return end
 	for _,f in pairs(triggers[e]) do
@@ -33,12 +30,26 @@ local function TriggerEvent(e)
 	end
 end
 
-local playerMana = nil
-local function UpdateMana()
-	local mana = UnitPower("player", 0)
-	local diff = mana - (playerMana or mana)
-	playerMana = mana
-	return diff
+local batch = {
+	spellCast = false,
+	lostMana = false,
+}
+
+local playerPower = {}
+local function UpdatePower(powerType)
+	local power = UnitPower("player", powerType)
+	local diff = power - (playerPower[powerType] or power)
+	playerPower[powerType] = power
+	if diff < 0 and powerType == 0 then
+		batch.lostMana = true
+	end
+	return diff > 0
+end
+local function UpdateHealth()
+	local health = UnitHealth("player")
+	local diff = health - (playerPower["hp"] or health)
+	playerPower["hp"] = health
+	return diff > 0
 end
 
 local function ManaTick(isReal)
@@ -47,36 +58,38 @@ local function ManaTick(isReal)
 	predictedTick = GetTime() + tickInterval
 end
 
-local batch = {
-	spellCast = false,
-	lostMana = false,
-}
-
 local f = CreateFrame("Frame")
 
 f:SetScript("OnEvent", function(self, e, ...)
 	if e == "COMBAT_LOG_EVENT_UNFILTERED" then
 		local a = {CombatLogGetCurrentEventInfo()}
 		if a[2] == "SPELL_ENERGIZE" or a[2] == "SPELL_PERIODIC_ENERGIZE" then
-			if a[8] == playerGUID and a[17] == 0 then -- a[17] is powerType
+			if a[8] == playerGUID --[[and a[17] == 0]] then -- a[17] is powerType
 				energizeRejectUntil = GetTime() + batchWindow + batchError -- based on Life Tap test
 			end
 		elseif a[2] == "SPELL_LEECH" or a[2] == "SPELL_PERIODIC_LEECH" then
-			if a[4] == playerGUID and a[16] == 0 then -- a[16] is powerType
+			if a[4] == playerGUID --[[and a[16] == 0]] then -- a[16] is powerType
 				energizeRejectUntil = GetTime() + 3 * batchWindow + batchError -- based on Dark Pact test
 			end
 		elseif a[2] == "SPELL_CAST_SUCCESS" then
 			if a[4] == playerGUID then
 				batch.spellCast = true
 			end
+		elseif a[2] == "SPELL_HEAL" or a[2] == "SPELL_PERIODIC_HEAL" then
+			if a[8] == playerGUID then
+				healRejectUntil = GetTime() + batchWindow + batchError
+			end
 		end
 	elseif e == "UNIT_POWER_UPDATE" then
 		if ... ~= "player" then return end
-		local manadiff = UpdateMana()
-		if manadiff < 0 then batch.lostMana = true end
-		if manadiff <= 0 then return end
-		if GetTime() <= energizeRejectUntil then return end
+		local gainedPower = UpdatePower(0) or UpdatePower(3)
+		if not gainedPower or GetTime() <= energizeRejectUntil then return end
 		ManaTick(true)
+	elseif e == "UNIT_HEALTH_FREQUENT" then
+		if ... ~= "player" then return end
+		local gainedHealth = UpdateHealth()
+		if not gainedHealth or GetTime() <= healRejectUntil then return end
+		ManaTick(false)
 	end
 end)
 
@@ -96,3 +109,4 @@ end)
 
 f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 f:RegisterEvent("UNIT_POWER_UPDATE")
+f:RegisterEvent("UNIT_HEALTH_FREQUENT")
